5b70cb7f31bd0aa5be6e18d7169bd4d44ba73ea5
Ethan Harsh <ethan@ethanharsh.com>
2021-06-19 21:14:43 +0000

    dynamically generated cards

--- public/main/portfolio/script.js
+++ public/main/portfolio/script.js
@@ -5,5 +5,12 @@ fetch('/api/v1/projects/all')
       
       data.data.projects.forEach(el => {
           console.log(el);
+          renderProjectCards(el);
       })
-  });
+});
+
+function renderProjectCards(project) {
+    let item = `<div class="card ${project.slug}"><figure class="display-box"><img src="${project.heroimg}" alt="card-img"></figure><div class="info-box"><h3><strong>${project.name}</strong></h3><p class="hover-hide">${project.description}</p></div><a class="button repo" href="${project.repo}">Git Repository</a><a class="button" href="${project.url}">View Project</a></div>`
+
+    $('#project-section').append(item);
+}
